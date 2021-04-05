3a58e267b33312cd5a38533c22d810f63c244957
Sarmad-47 <80609603+Sarmad-47@users.noreply.github.com>
2021-04-05 11:19:03 +0500

    Add files via upload

---
+++ SP20-BSE-084_LAB6_ASSIGNMENTS/lab6_a1/CalculatorRunner.java
@@ -0,0 +1,17 @@
+package lab6_a1;
+
+public class CalculatorRunner {
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.num1 =12 );
+        System.out.println(Calculator.num2 = 24);
+        Calculator.Sum();
+        Calculator.Multiply();
+        Calculator.Divide();
+        Calculator.Modulus();
+          Calculator.Sin();
+          Calculator.Cos();
+          Calculator.Tan();
+    }
+
+}
